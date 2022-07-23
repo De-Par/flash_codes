88d5336694c08bd3c3d9a0207481deb04feb8c02
Daniil <60934755+De-Par@users.noreply.github.com>
2022-07-24 00:23:36 +0300

    Add files via upload

---
+++ cases_for_competition/case_17/variant_0/17_pro_0.py
@@ -0,0 +1,8 @@
+p = [int(x) for x in open('17_file_0.txt')]
+count = 0; mx = float('-inf')
+for a,b,c in zip(p, p[1:], p[2:]):
+    mx_n =  max(a,b,c)
+    if mx_n**2 == (a+b+c-mx_n)**2 - 2*a*b*c/mx_n:
+        count += 1
+        mx = max(mx, a*b*c*0.5/mx_n)
+print(count, mx)
