c4972022d6d6b5f80ac02ade481c803d1d9659c4
Daniil <60934755+De-Par@users.noreply.github.com>
2022-07-29 00:19:17 +0300

    Delete 17_pro_0.py

--- cases_for_competition/case_17/variant_0/17_pro_0.py
+++
@@ -1,8 +0,0 @@
-p = [int(x) for x in open('17_file_0.txt')]
-count = 0; mx = float('-inf')
-for a,b,c in zip(p, p[1:], p[2:]):
-    mx_n =  max(a,b,c)
-    if mx_n**2 == (a+b+c-mx_n)**2 - 2*a*b*c/mx_n:
-        count += 1
-        mx = max(mx, a*b*c*0.5/mx_n)
-print(count, mx)
